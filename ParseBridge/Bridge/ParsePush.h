8fef11fa428da4bb66e91daaaeb4ced8f0223f83
Spencer Barron <contact@spencerbarron.com>
2013-08-13 02:09:13 -0600

    Added ParsePush
    Added ParsePush.  Modified configuration.json sources to include
    current bridge .m files

---
+++ ParseBridge/Bridge/ParsePush.h
@@ -0,0 +1,13 @@
+//
+//  ParsePush.h
+//  ParseBridge
+//
+//  Created by Spencer Barron on 8/13/13.
+//  Copyright (c) 2013 Apportable. All rights reserved.
+//
+
+#import <BridgeKit/JavaObject.h>
+
+@interface ParsePush : JavaObject
+
+@end
---
+++ ParseBridge/Bridge/ParsePush.m
@@ -0,0 +1,83 @@
+//
+//  ParsePush.m
+//  ParseBridge
+//
+//  Created by Spencer Barron on 8/13/13.
+//  Copyright (c) 2013 Apportable. All rights reserved.
+//
+
+#import "ParsePush.h"
+
+@implementation ParsePush
+
++ (void)initializeJava
+{
+    [super initializeJava];
+	
+	//Constructor
+	//*- Java: ParsePush()
+	/*
+	Creates a new push notification. The default channel is the empty string, also known as the global broadcast channel, but this value can be overridden using ParsePush.setChannel(String), ParsePush.setChannels(Collection) or ParsePush.setQuery(ParseQuery). Before sending the push notification you must call either ParsePush.setMessage(String) or ParsePush.setData(JSONObject).
+	*/
+	[ParsePush registerConstructor];
+	
+	//*- Java:  public static void sendMessageInBackground(String message, ParseQuery<ParseInstallation> query)
+	//A helper method to concisely send a push message to a query. This method is equivalent to ParsePush push = new ParsePush(); push.setMessage(message); push.setQuery(query); push.sendInBackground();
+ 
+
+//*- Java:  public static void sendMessageInBackground(String message, ParseQuery<ParseInstallation> query,SendCallback callback)
+	//A helper method to concisely send a push message to a query. This method is equivalent to ParsePush push = new ParsePush(); push.setMessage(message); push.setQuery(query); push.sendInBackground(callback);
+ 
+//*- Java:  public static void sendDataInBackground(JSONObject data,ParseQuery<ParseInstallation> query)
+    //A helper method to concisely send a push to a query. This method is equivalent to ParsePush push = new ParsePush(); push.setData(data); push.setQuery(query); push.sendInBackground();
+ 
+//*- Java:  public static void sendDataInBackground(JSONObject data,ParseQuery<ParseInstallation> query,SendCallback callback)
+	//A helper method to concisely send a push to a query. This method is equivalent to ParsePush push = new ParsePush(); push.setData(data); push.setQuery(query); push.sendInBackground(callback);
+ 
+
+//*- Java:  public void setChannel(String channel)
+	//Sets the channel on which this push notification will be sent. The channel name must start with a letter and contain only letters, numbers, dashes, and underscores. A push can either have channels or a query. Setting this will unset the query.
+
+//*- Java:  public void setChannels(Collection<String> channels)
+	//Sets the collection of channels on which this push notification will be sent. Each channel name must start with a letter and contain only letters, numbers, dashes, and underscores. A push can either have channels or a query. Setting this will unset the query.
+ 
+
+//*- Java:  public void setQuery(ParseQuery<ParseInstallation> query)
+	//Sets the query for this push for which this push notification will be sent. This query will be executed in the Parse cloud; this push notification will be sent to Installations which this query yields. A push can either have channels or a query. Setting this will unset the channels.
+ 
+//*- Java:  public void setExpirationTime(long time)
+	//Sets a UNIX epoch timestamp at which this notification should expire, in seconds (UTC). This notification will be sent to devices which are either online at the time the notification is sent, or which come online before the expiration time is reached. Because device clocks are not guaranteed to be accurate, most applications should instead use ParsePush.setExpirationTimeInterval(long).
+ 
+//*- Java:  public void setExpirationTimeInterval(long timeInterval)
+	//Sets the time interval after which this notification should expire, in seconds. This notification will be sent to devices which are either online at the time the notification is sent, or which come online within the given number of seconds of the notification being received by Parse's server. An interval which is less than or equal to zero indicates that the message should only be sent to devices which are currently online.
+ 
+//*- Java:  public void clearExpiration()
+	//Clears both expiration values, indicating that the notification should never expire.
+
+
+//*- Java:  public void setData(JSONObject data)
+	//Sets the entire data of the push message. See the push guide for more details on the data format. This will overwrite any data specified in ParsePush.setMessage(String).
+ 
+
+//*- Java:  public void setMessage(String message)
+	//Sets the message that will be shown in the notification. This will overwrite any data specified in ParsePush.setData(JSONObject).
+ 
+//*- Java:  public void send()   throws ParseException 
+	//Sends this push notification while blocking this thread until the push notification has successfully reached the Parse servers. Typically, you should use ParsePush.sendInBackground() instead of this, unless you are managing your own threading.
+ 
+//*- Java:  public void sendInBackground(SendCallback callback)
+	//Sends this push notification in a background thread. This is preferable to using send(), unless your code is already running from a background thread.
+ 
+
+//*- Java:  public void sendInBackground()
+	//Sends this push notification in a background thread. Use this when you do not have code to run on completion of the push.
+
+	
+}
+
++ (NSString *)className
+{
+    return @"com.parse.ParsePush";
+}
+
+@end
